1b3adeed61c9d9b2dd82f3912d63e335946387fe
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 08:32:16 +0200

    routing

---
+++ src/views/contact.js
@@ -0,0 +1,9 @@
+import {Container} from "react-bootstrap";
+function Contact(){
+    return(        
+        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
+            Contact
+        </Container>
+    );
+}
+export default Contact;
